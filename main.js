430d3b8bcf8054823c35b6312918578bc153d7f5
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-23 22:02:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-const keyElements = document.querySelectorAll('.key');
+const myPiano = () => {
+    const keyElements = document.querySelectorAll('.key');
 
-const createDivBlack = () => {
-    for (let index = 0; index < 10; index++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('key', 'black_key');//10
-        const blackKeys = document.querySelector('.black_keys');
-        blackKeys.appendChild(newDiv);
-    }
-}
+        const createDivBlack = () => {
+        for (let index = 0; index < 10; index++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('key', 'black_key'); //10
+            const blackKeys = document.querySelector('.black_keys');
+            blackKeys.appendChild(newDiv);
+        }
+    };
 
-const createDivWhite = () => {
-    for (let index = 0; index < 14; index++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('key', 'white_key');//14
-        const whiteKeys = document.querySelector('.white_keys');
-        whiteKeys.appendChild(newDiv);
-    }
-}
+    const createDivWhite = () => {
+        for (let index = 0; index < 14; index++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('key', 'white_key'); //14
+            const whiteKeys = document.querySelector('.white_keys');
+            whiteKeys.appendChild(newDiv);
+        }
+    };
 
-const playSound = (newUrl) => {
-    const audio = new Audio(newUrl);
-    audio.play();
+    const playSound = (newUrl) => {
+        const audio = new Audio(newUrl);
+        audio.play();
+    };
+
+    // createDivBlack();
+    // createDivWhite();
+
+    keyElements.forEach((pianoKey, i) => {
+        const number = i < 9 ? '0' + (i + 1) : i + 1;
+        const newUrl = `sounds/key${number}.mp3`;
+        console.log(newUrl);
+        pianoKey.addEventListener('click', () => {
+            playSound(newUrl);
+        });
+    });
 };
 
-// createDivBlack();
-// createDivWhite();
-
-keyElements.forEach((pianoKey, i) => {
-    const number = i < 9 ? '0' + (i + 1) : i + 1;
-    const newUrl = `sounds/key${number}.mp3`;
-    console.log(newUrl);
-    pianoKey.addEventListener('click', () => {
-        console.log('clicked');
-        playSound(newUrl);
-    });
-});
+myPiano();
