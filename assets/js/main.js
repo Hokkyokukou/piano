3f44922cd2b6f6f6b43e99d35d5a032b6702b763
Hokkyokukou <hokkyokukou27@gmail.com>
2023-05-03 23:05:00 +0200

    removed commented code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,15 @@ import { createDivBlack, createDivWhite } from './modules/keys.js';
 const myPiano = () => {
 
     document.addEventListener('DOMContentLoaded', () => {
+
         createDivBlack();
         createDivWhite();
 
         const keyElements = document.querySelectorAll('.key');
-        // console.log(keyElements);
-
+        
         keyElements.forEach((pianoKey, i) => {
             const number = i < 9 ? '0' + (i + 1) : i + 1;
             const newUrl = `assets/sounds/key${number}.mp3`;
-            // console.log(newUrl);
             pianoKey.addEventListener('click', () => {
                 playSound(newUrl);
             });
